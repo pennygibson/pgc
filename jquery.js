d7d1493021a4b1fc639579f9acbc10ba4847c221
pennygibson <cenneboulet@yahoo.ca>
2018-05-15 00:22:24 +0000

    initial commit of finished site

---
+++ jquery.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.pink').delay(500).fadeIn(2000);
+    $('.blue').delay(500).fadeIn(4000);
+    $('.yellow').delay(500).fadeIn(6000);
+    $('.grey').delay(500).fadeIn(8000);
+	
+	$( "#button1" ).click(function() {
+  $( ".section1").slideToggle( 800 );
+});
+	$( "#button2" ).click(function() {
+  $( ".section2").slideToggle( 800 );
+});
+	$( "#button3" ).click(function() {
+  $( ".section3").slideToggle( 800 );
+});
+	$( "#button4" ).click(function() {
+  $( ".section4").slideToggle( 800 );
+});
+	$( "#button5" ).click(function() {
+  $( ".section5").slideToggle( 800 );
+});
+	$( "#button6" ).click(function() {
+  $( ".section6").slideToggle( 800 );
+});
+	$( "#button7" ).click(function() {
+  $( ".section7").slideToggle( 800 );
+});
+  });
